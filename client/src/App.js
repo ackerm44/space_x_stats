05ca98770971de215c5b8da21cfe0f52ab75ba88
ackerm44 <ackerm44@gmail.com>
2018-03-13 20:16:01 -0400

    Added react-router-dom, updated npm, added basic routes to App.js for root and rockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom'
 import './App.css';
 import LatestLaunch from './Launches/LatestLaunch'
 import Rockets from './containers/Rockets'
@@ -9,12 +10,25 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <LatestLaunch />
-        <Rockets />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={LatestLaunch} />
+          <Route exact path="/rockets" component={Rockets} />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+// const App = (props) =>
+//   <Router>
+//     <div>
+//     // NavLink
+//       <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
+//       <Route path="/pets" component={PetsPage} />
+//     </div>
+//   </Router>;
+//
+// export default App;
