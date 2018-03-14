d600fac6e0f3306b14050b719228c8711e495fb1
ackerm44 <ackerm44@gmail.com>
2018-03-14 12:02:45 -0400

    Created news reducers and added to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,6 +4,7 @@ import { rocketsHasErrored, rocketsIsLoading, rockets} from './rockets'
 import { launchpadsHasErrored, launchpadsIsLoading, launchpads} from './launchpads'
 import { upcomingLaunchesHasErrored, upcomingLaunchesIsLoading, upcomingLaunches} from './upcomingLaunches'
 import { pastLaunchesHasErrored, pastLaunchesIsLoading, pastLaunches} from './pastLaunches'
+import { newsHasErrored, newsIsLoading, news} from "./news"
 
 export default combineReducers({
     latestLaunch,
@@ -20,5 +21,8 @@ export default combineReducers({
     upcomingLaunches,
     pastLaunchesHasErrored,
     pastLaunchesIsLoading,
-    pastLaunches
+    pastLaunches,
+    newsHasErrored,
+    newsIsLoading,
+    news
 });
